112249f93059aa8c498040a6fb8ff24514b72db1
Leo <leonlevzenko@gmail.com>
2025-09-25 21:14:34 +0300

    добавил файлы по умолчанию

---
+++ src/main/kotlin/Serialization.kt
@@ -0,0 +1,25 @@
+package com.FastAndCurious
+
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.html.*
+import io.ktor.server.plugins.calllogging.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.openapi.*
+import io.ktor.server.plugins.swagger.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import java.sql.Connection
+import java.sql.DriverManager
+import kotlinx.html.*
+import org.slf4j.event.*
+
+fun Application.configureSerialization() {
+    routing {
+        get("/json/kotlinx-serialization") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+}
